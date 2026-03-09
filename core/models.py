147e139418a1319f1beb2a68bba4cb17350a2741
Rafael <rafael1893porto@gmail.com>
2026-03-09 09:36:32 +0100

    add Person class

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+#person class
+class Person(models.Model):
+    #user to be associated
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+
