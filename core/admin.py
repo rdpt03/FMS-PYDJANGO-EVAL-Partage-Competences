1ad0f5eea04348fba6822ead78a65f248cd3b262
Rafael <rafael1893porto@gmail.com>
2026-03-10 15:31:40 +0100

    simplify person/user creation

--- core/admin.py
+++ core/admin.py
@@ -7,34 +7,21 @@ from .models import Person, Skill, Task
 # Register your models here.
 
 # --- Form pour user + Person ---
-class PersonCreationForm(forms.ModelForm):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    email = forms.EmailField(required=False)
+# --- Admin de Person ---
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'user')
+    search_fields = ('first_name', 'last_name', 'user__username')
+    # Sem form custom: cria apenas campos da Person
+    # Usuário deve ser criado antes no admin padrão User
 
-    class Meta:
-        model = Person
-        fields = ['first_name', 'last_name', 'phone_num', 'address', 'skills']
 
-    def save(self, commit=True):
-        # create User before using core Django
-        user = User.objects.create_user(
-            username=self.cleaned_data['username'],
-            password=self.cleaned_data['password'],
-            email=self.cleaned_data.get('email', '')
-        )
-        # Save user and associate to User
-        person = super().save(commit=False)
-        person.user = user
-        if commit:
-            person.save()
-            self.save_m2m()  # save ManyToManyField skills
-        return person
 # --- Admin de Skill ---
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
 
 # --- Admin de Task ---
 @admin.register(Task)
