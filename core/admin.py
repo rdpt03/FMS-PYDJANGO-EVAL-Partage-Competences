60fab094f8efc7da9ead75e932cc4e1ad86dce10
Rafael <rafael1893porto@gmail.com>
2026-03-10 15:11:03 +0100

    add Skill + Task form

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django import forms
-from .models import Person
+from .models import Person, Skill, Task
+
 
 # Register your models here.
 
@@ -28,4 +30,16 @@ class PersonCreationForm(forms.ModelForm):
             person.save()
             self.save_m2m()  # save ManyToManyField skills
         return person
+# --- Admin de Skill ---
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# --- Admin de Task ---
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task_type', 'skill', 'requester', 'helper', 'start_date', 'end_date')
+    list_filter = ('task_type', 'skill')
+    search_fields = ('title', 'description', 'requester__first_name', 'requester__last_name')
 
