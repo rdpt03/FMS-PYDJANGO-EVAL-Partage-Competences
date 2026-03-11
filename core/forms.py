24811941e57745b85c75d09976757c2acb948fd1
Rafael <rafael1893porto@gmail.com>
2026-03-11 22:24:00 +0100

    Add form to create task

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Person, Skill
+from .models import Person, Skill, Task
+
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
@@ -14,3 +15,14 @@ class RegisterForm(UserCreationForm):
         model = User
 
         fields = ["username", "email", "password1", "password2"]
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description", "start_date", "end_date"]
+
+        widgets = {
+            "start_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "end_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        }
