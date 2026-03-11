a13d8f4ad17aaf41dd8cf9f410bd18aa291430aa
Rafael <rafael1893porto@gmail.com>
2026-03-11 11:36:55 +0100

    add url

--- echange_competences/urls.py
+++ echange_competences/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('skills', views.skills, name='skills'),
+    path('tasks', views.tasks, name='tasks'),
 ]
