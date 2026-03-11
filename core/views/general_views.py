b89f2012f4547b1f413306fc48d0f24f8da342e8
Rafael <rafael1893porto@gmail.com>
2026-03-11 22:25:40 +0100

    handle guest and users on main homepage to redirect to the correct homepage if logged or not

--- core/views/general_views.py
+++ core/views/general_views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
-    return render(request, 'base_disconnected.html')
+    if request.user.is_authenticated:
+        return redirect("skills_disconnected")
+    else:
+        return redirect("skills_connected")#TODO temporary - replace with help asked
+    #return render(request, 'base_disconnected.html')
