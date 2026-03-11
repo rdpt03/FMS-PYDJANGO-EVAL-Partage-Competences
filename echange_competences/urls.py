c4ab6aa7521d812ec84dce2f251c61c6475d8950
Rafael <rafael1893porto@gmail.com>
2026-03-11 12:59:06 +0100

    add / to urls

--- echange_competences/urls.py
+++ echange_competences/urls.py
@@ -22,6 +22,6 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('skills', views.skills, name='skills'),
-    path('tasks', views.tasks, name='tasks'),
+    path('skills/', views.skills, name='skills'),
+    path('tasks/', views.tasks, name='tasks'),
 ]
