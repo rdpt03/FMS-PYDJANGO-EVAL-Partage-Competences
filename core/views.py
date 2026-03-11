037d93ac4fd3871f35fc9d795f406d261903abff
Rafael <rafael1893porto@gmail.com>
2026-03-11 11:36:03 +0100

    add option to see tasks on logged out users
    this only allows to see anonymous tasks that going to happen and have no helper

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.utils import timezone
 
-from core.models import Skill
+from core.models import Skill, Task
+
 
 # Create your views here.
 def home(request):
@@ -12,7 +14,6 @@ def skills(request):
     #get all skills
     all_skills = Skill.objects.all().order_by('name')
 
-    print(Skill.objects.all().order_by('name'))
     #get page number
     page_num = request.GET.get('page')
     #gets quantity per page
@@ -28,3 +29,29 @@ def skills(request):
     page_skills = paginator.get_page(page_num)
 
     return render(request, "disconnected_pages/skills_list.html", {"page_skills": page_skills, "quantity_per_page":quantity_per_page,})
+
+
+def tasks(request):
+    # get all tasks
+    all_tasks = Task.objects.all().filter(
+        helper__isnull=True,
+        task_type=Task.TaskType.REQUEST,
+        start_date__gt=timezone.now()
+    ).order_by('-published_date')
+
+    # get page number
+    page_num = request.GET.get('page')
+    # gets quantity per page
+    try:
+        quantity_per_page = int(request.GET.get('quantity', 20))
+        if quantity_per_page <= 0:
+            quantity_per_page = 20
+    except ValueError:
+        quantity_per_page = 20
+
+    # paginator
+    paginator = Paginator(all_tasks, quantity_per_page)
+    page_tasks = paginator.get_page(page_num)
+
+    return render(request, "disconnected_pages/tasks_lists.html",
+                  {"page_tasks": page_tasks, "quantity_per_page": quantity_per_page, })
